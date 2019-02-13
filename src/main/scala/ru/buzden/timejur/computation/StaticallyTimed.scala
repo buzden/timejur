47189ee1ace574e439358820a8e4c4c956597c20
Denis Buzdalov <public@buzden.ru>
2019-02-13 19:19:41 +0300

    General `TimedComputation` was renamed to `StaticallyTimed`.
    This is done because time in this data structure does not depend on the
    input of the computation, thus it is fixed for all inputs and can be
    statically determined.

---
+++ src/main/scala/ru/buzden/timejur/computation/StaticallyTimed.scala
@@ -0,0 +1,45 @@
+package ru.buzden.timejur.computation
+
+import cats.arrow.{Arrow, ArrowChoice}
+import cats.instances.function._
+import cats.syntax.order._
+import cats.syntax.semigroup._
+import cats.{Contravariant, Functor, Monoid, Order}
+
+case class StaticallyTimed[A, B, T](computation: A => B, time: T)
+
+object StaticallyTimed extends StaticallyTimed0Instances {
+  implicit def tcFunctor[X, T]: Functor[StaticallyTimed[X, ?, T]] = new Functor[StaticallyTimed[X, ?, T]] {
+    override def map[A, B](fa: StaticallyTimed[X, A, T])(f: A => B): StaticallyTimed[X, B, T] =
+      StaticallyTimed(fa.computation `andThen` f, fa.time)
+  }
+
+  implicit def tcContravariant[Y, T]: Contravariant[StaticallyTimed[?, Y, T]] = new Contravariant[StaticallyTimed[?, Y, T]] {
+    override def contramap[A, B](fa: StaticallyTimed[A, Y, T])(f: B => A): StaticallyTimed[B, Y, T] =
+      StaticallyTimed(fa.computation `compose` f, fa.time)
+  }
+
+  implicit def tcArrowChoice[T: Monoid:Order]: Arrow[StaticallyTimed[?, ?, T]] = new TCArrowChoice[T]
+}
+
+trait StaticallyTimed0Instances {
+  implicit def tcArrow[T: Monoid]: Arrow[StaticallyTimed[?, ?, T]] = new TCArrow[T]
+}
+
+private class TCArrow[T: Monoid] extends Arrow[StaticallyTimed[?, ?, T]] {
+  /** Simple type alias for the sake of tacitness */
+  type =|>[A, B] = StaticallyTimed[A, B, T]
+
+  override def lift[A, B](f: A => B): A =|> B = StaticallyTimed(f, Monoid[T].empty)
+
+  override def compose[A, B, C](f: B =|> C, g: A =|> B): A =|> C =
+    StaticallyTimed(f.computation `compose` g.computation, f.time |+| g.time)
+
+  override def first[A, B, C](fa: A =|> B): (A, C) =|> (B, C) =
+    StaticallyTimed(Arrow[Function1].first(fa.computation), fa.time)
+}
+
+private class TCArrowChoice[T: Monoid:Order] extends TCArrow[T] with ArrowChoice[StaticallyTimed[?, ?, T]] {
+  override def choose[A, B, C, D](f: A =|> C)(g: B =|> D): Either[A, B] =|> Either[C, D] =
+    StaticallyTimed(ArrowChoice[Function1].choose(f.computation)(g.computation), f.time `max` g.time)
+}
