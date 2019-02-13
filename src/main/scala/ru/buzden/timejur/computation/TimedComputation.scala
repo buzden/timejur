2c6b9c0e8ca54068e9745d9d73092d4fb5c589ed
Denis Buzdalov <public@buzden.ru>
2019-02-13 17:12:16 +0300

    Contravariant and covariant functors instances were added.

--- src/main/scala/ru/buzden/timejur/computation/TimedComputation.scala
+++ src/main/scala/ru/buzden/timejur/computation/TimedComputation.scala
@@ -1,6 +1,17 @@
 package ru.buzden.timejur.computation
 
+import cats.{Contravariant, Functor}
+
 case class TimedComputation[A, B, T](computation: A => B, time: T)
 
 object TimedComputation {
+  implicit def tcFunctor[X, T]: Functor[TimedComputation[X, ?, T]] = new Functor[TimedComputation[X, ?, T]] {
+    override def map[A, B](fa: TimedComputation[X, A, T])(f: A => B): TimedComputation[X, B, T] =
+      TimedComputation(fa.computation `andThen` f, fa.time)
+  }
+
+  implicit def tcContravariant[Y, T]: Contravariant[TimedComputation[?, Y, T]] = new Contravariant[TimedComputation[?, Y, T]] {
+    override def contramap[A, B](fa: TimedComputation[A, Y, T])(f: B => A): TimedComputation[B, Y, T] =
+      TimedComputation(fa.computation `compose` f, fa.time)
+  }
 }
