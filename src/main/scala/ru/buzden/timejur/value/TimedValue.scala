c91d767f14dd6b9e9f3d3f6e606af06efc7f148a
Denis Buzdalov <public@buzden.ru>
2019-02-14 23:44:08 +0300

    Semigroup instance was added for timed value.

--- src/main/scala/ru/buzden/timejur/value/TimedValue.scala
+++ src/main/scala/ru/buzden/timejur/value/TimedValue.scala
@@ -1,3 +1,13 @@
 package ru.buzden.timejur.value
 
+import cats.Semigroup
+import cats.syntax.semigroup._
+
 final case class TimedValue[+A, +T](value: A, time: T)
+
+object TimedValue extends TimedValueInstances
+
+trait TimedValueInstances {
+  implicit def tvSemigroup[A: Semigroup, T: Semigroup]: Semigroup[TimedValue[A, T]] = (tv1, tv2) =>
+    TimedValue(tv1.value |+| tv2.value, tv1.time |+| tv2.time)
+}
