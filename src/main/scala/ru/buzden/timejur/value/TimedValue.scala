d482cb13c43ae551ab61d962d824cbb3bcd23431
Denis Buzdalov <public@buzden.ru>
2019-02-14 23:47:17 +0300

    Functor instance for timed value was added.

--- src/main/scala/ru/buzden/timejur/value/TimedValue.scala
+++ src/main/scala/ru/buzden/timejur/value/TimedValue.scala
@@ -1,7 +1,7 @@
 package ru.buzden.timejur.value
 
-import cats.Semigroup
 import cats.syntax.semigroup._
+import cats.{Functor, Semigroup}
 
 final case class TimedValue[+A, +T](value: A, time: T)
 
@@ -10,4 +10,9 @@ object TimedValue extends TimedValueInstances
 trait TimedValueInstances {
   implicit def tvSemigroup[A: Semigroup, T: Semigroup]: Semigroup[TimedValue[A, T]] = (tv1, tv2) =>
     TimedValue(tv1.value |+| tv2.value, tv1.time |+| tv2.time)
+
+  implicit def tvFunctor[T]: Functor[TimedValue[?, T]] = new Functor[TimedValue[?, T]] {
+    override def map[A, B](fa: TimedValue[A, T])(f: A => B): TimedValue[B, T] =
+      TimedValue(f(fa.value), fa.time)
+  }
 }
