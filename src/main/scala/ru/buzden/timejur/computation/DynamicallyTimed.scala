896d02e3e467728312ae0508b4b0fee0d47e680b
Denis Buzdalov <public@buzden.ru>
2019-02-14 16:13:31 +0300

    Instances for covariant and contravariant functors were added.

--- src/main/scala/ru/buzden/timejur/computation/DynamicallyTimed.scala
+++ src/main/scala/ru/buzden/timejur/computation/DynamicallyTimed.scala
@@ -1,5 +1,7 @@
 package ru.buzden.timejur.computation
 
+import cats.{Contravariant, Functor}
+
 /**
   * Data structure representing a timed computation (i.e., a computation
   * that spends some model time to get its result) where
@@ -12,3 +14,18 @@ package ru.buzden.timejur.computation
   * @param f function that defines computation **and** spent model time for each input
   */
 final case class DynamicallyTimed[-A, +B, +T](f: A => (B, T))
+
+object DynamicallyTimed {
+  implicit def dtFunctor[X, T]: Functor[DynamicallyTimed[X, ?, T]] = new Functor[DynamicallyTimed[X, ?, T]] {
+    override def map[A, B](fa: DynamicallyTimed[X, A, T])(f: A => B): DynamicallyTimed[X, B, T] =
+      DynamicallyTimed { x =>
+        val (intermediate, time) = fa.f(x)
+        (f(intermediate), time)
+      }
+  }
+
+  implicit def dtContravariant[Y, T]: Contravariant[DynamicallyTimed[?, Y, T]] = new Contravariant[DynamicallyTimed[?, Y, T]] {
+    override def contramap[A, B](fa: DynamicallyTimed[A, Y, T])(f: B => A): DynamicallyTimed[B, Y, T] =
+      DynamicallyTimed(fa.f `compose` f)
+  }
+}
