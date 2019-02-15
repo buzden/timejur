40133ccf1b9944cd3dade2d32fe46de892b447b3
Denis Buzdalov <public@buzden.ru>
2019-02-15 22:38:25 +0300

    (Controversal) syntax was added for indexed monoid.

--- src/main/scala/ru/buzden/typelevel/IndexedMonoid.scala
+++ src/main/scala/ru/buzden/typelevel/IndexedMonoid.scala
@@ -5,3 +5,9 @@ trait IndexedMonoid[T] extends IndexedSemigroup[T] {
 
   def empty: Empty
 }
+
+object IndexedMonoid {
+  object syntax {
+    def empty[T](implicit im: IndexedMonoid[T]): im.Empty = im.empty
+  }
+}
