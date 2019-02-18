7e77af77436789d54e4ffcbb45c676de90ef22fa
Denis Buzdalov <public@buzden.ru>
2019-02-18 15:13:17 +0300

    Indexed timed value has been reimplemented using general indexed monad.

--- src/main/scala/ru/buzden/timejur/value/IndexedTimedValue.scala
+++ src/main/scala/ru/buzden/timejur/value/IndexedTimedValue.scala
@@ -1,8 +1,17 @@
 package ru.buzden.timejur.value
 
-import ru.buzden.typelevel.IndexingSemigroup
+import ru.buzden.typelevel._
 
-final case class IndexedTimedValue[A, T, ATime <: T with Singleton](value: A) {
-  def flatMap[B, BTime <: T with Singleton](f: A => IndexedTimedValue[B, T, BTime])(is: IndexingSemigroup[T]): IndexedTimedValue[B, T, is.|+|[ATime, BTime]] =
-    IndexedTimedValue[B, T, is.|+|[ATime, BTime]](f(value).value)
+final case class IndexedTimedValue[A, T, ATime <: T with Singleton](value: A)
+
+object IndexedTimedValue {
+  implicit val indexedMonadForIndexedTimedValue: IndexedMonad[IndexedTimedValue] = new IndexedMonad[IndexedTimedValue] {
+    override def pure[T, A](a: A)(implicit im: IndexingMonoid[T]): IndexedTimedValue[A, T, im.Empty] =
+      IndexedTimedValue[A, T, im.Empty](a)
+
+    override def flatMap[T, A, ATime <: T with Singleton, B, BTime <: T with Singleton]
+        (fa: IndexedTimedValue[A, T, ATime])(f: A => IndexedTimedValue[B, T, BTime])
+        (implicit is: IndexingSemigroup[T]): IndexedTimedValue[B, T, is.|+|[ATime, BTime]] =
+      IndexedTimedValue[B, T, is.|+|[ATime, BTime]](f(fa.value).value)
+  }
 }
