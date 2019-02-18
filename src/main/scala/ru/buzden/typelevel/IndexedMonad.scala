03f227f321d1df314163d9104a614af5e6a7f1ad
Denis Buzdalov <public@buzden.ru>
2019-02-18 14:46:02 +0300

    Some syntax object was added for the indexed monad.

--- src/main/scala/ru/buzden/typelevel/IndexedMonad.scala
+++ src/main/scala/ru/buzden/typelevel/IndexedMonad.scala
@@ -6,3 +6,17 @@ trait IndexedMonad[F[_, _]] {
   def flatMap[I, A, I_A <: I with Singleton, B, I_B <: I with Singleton]
     (fa: F[A, I_A])(f: A => F[B, I_B])(implicit is: IndexingSemigroup[I]): F[B, is.|+|[I_A, I_B]]
 }
+
+object IndexedMonad {
+  object syntax {
+    implicit class IndexedMonadAnyAOps[A](val a: A) extends AnyVal {
+      def pure[F[_, _], I](implicit iM: IndexedMonad[F], im: IndexingMonoid[I]): F[A, im.Empty] = iM.pure(a)(im)
+    }
+
+    implicit class IndexedMonadOps[F[_, _], A, I, I_A <: I with Singleton](val fa: F[A, I_A]) extends AnyVal {
+      def flatMap[B, I_B <: I with Singleton]
+          (f: A => F[B, I_B])(implicit iM: IndexedMonad[F], is: IndexingSemigroup[I]): F[B, is.|+|[I_A, I_B]] =
+        iM.flatMap[I, A, I_A, B, I_B](fa)(f)(is)
+    }
+  }
+}
