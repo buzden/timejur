f24a788ecaeb10dd4b10ca02a7ebd31933e05aa2
Denis Buzdalov <public@buzden.ru>
2019-02-18 18:28:32 +0300

    Indexing monoid instance was added for `Unit` (with coercion, however).

--- src/main/scala/ru/buzden/typelevel/IndexingMonoid.scala
+++ src/main/scala/ru/buzden/typelevel/IndexingMonoid.scala
@@ -10,4 +10,14 @@ object IndexingMonoid {
   object syntax {
     def empty[T](implicit im: IndexingMonoid[T]): im.Empty = im.empty
   }
+
+  implicit val unitHasIndexingMonoid: IndexingMonoid[Unit] = new IndexingMonoid[Unit] {
+    val u: X[Unit] = ().asInstanceOf[X[Unit]] // I don't know why I need to coerce here.
+
+    override type Empty = X[Unit]
+    override def empty: Empty = u
+
+    override type |+|[A, B] = X[Unit]
+    override def combine[A <: X[Unit], B <: X[Unit]](a: A, b: B): A |+| B = u
+  }
 }
