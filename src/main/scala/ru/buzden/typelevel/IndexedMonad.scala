0e223b3d8571df4ab2561f401f25dabb26794ec8
Denis Buzdalov <public@buzden.ru>
2019-02-18 15:00:17 +0300

    Indexed monad was changed to require a type with three holes, not two.

--- src/main/scala/ru/buzden/typelevel/IndexedMonad.scala
+++ src/main/scala/ru/buzden/typelevel/IndexedMonad.scala
@@ -1,21 +1,22 @@
 package ru.buzden.typelevel
 
-trait IndexedMonad[F[_, _]] {
-  def pure[I, A](a: A)(implicit im: IndexingMonoid[I]): F[A, im.Empty]
+trait IndexedMonad[F[_, _, _]] {
+  def pure[I, A](a: A)(implicit im: IndexingMonoid[I]): F[A, I, im.Empty]
 
   def flatMap[I, A, I_A <: I with Singleton, B, I_B <: I with Singleton]
-    (fa: F[A, I_A])(f: A => F[B, I_B])(implicit is: IndexingSemigroup[I]): F[B, is.|+|[I_A, I_B]]
+    (fa: F[A, I, I_A])(f: A => F[B, I, I_B])(implicit is: IndexingSemigroup[I]): F[B, I, is.|+|[I_A, I_B]]
 }
 
 object IndexedMonad {
   object syntax {
     implicit class IndexedMonadAnyAOps[A](val a: A) extends AnyVal {
-      def pure[F[_, _], I](implicit iM: IndexedMonad[F], im: IndexingMonoid[I]): F[A, im.Empty] = iM.pure(a)(im)
+      def pure[F[_, _, _], I](implicit iM: IndexedMonad[F], im: IndexingMonoid[I]): F[A, I, im.Empty] =
+        iM.pure(a)(im)
     }
 
-    implicit class IndexedMonadOps[F[_, _], A, I, I_A <: I with Singleton](val fa: F[A, I_A]) extends AnyVal {
+    implicit class IndexedMonadOps[F[_, _, _], A, I, I_A <: I with Singleton](val fa: F[A, I, I_A]) extends AnyVal {
       def flatMap[B, I_B <: I with Singleton]
-          (f: A => F[B, I_B])(implicit iM: IndexedMonad[F], is: IndexingSemigroup[I]): F[B, is.|+|[I_A, I_B]] =
+          (f: A => F[B, I, I_B])(implicit iM: IndexedMonad[F], is: IndexingSemigroup[I]): F[B, I, is.|+|[I_A, I_B]] =
         iM.flatMap[I, A, I_A, B, I_B](fa)(f)(is)
     }
   }
