5027a7787fe6c5f577735288f46566df4e6a79f6
Denis Buzdalov <public@buzden.ru>
2019-02-19 18:26:28 +0300

    Additional type parameter for one implicit value for `combine` was added

--- src/main/scala/ru/buzden/typelevel/IndexingSemigroup.scala
+++ src/main/scala/ru/buzden/typelevel/IndexingSemigroup.scala
@@ -1,15 +1,15 @@
 package ru.buzden.typelevel
 
-trait IndexingSemigroup[T] {
+trait IndexingSemigroup[T, ZZ[_, _]] {
   type |+|[A <: X[T], B <: X[T]] <: X[T]
 
-  def combine[A <: X[T], B <: X[T]]: A |+| B
+  def combine[A <: X[T], B <: X[T]](implicit i: ZZ[A, B]): A |+| B
 }
 
 object IndexingSemigroup {
   object syntax {
     implicit class IndexedSemigroupOps[T, A <: X[T]](val a: A) extends AnyVal {
-      def |+|[B <: X[T]](b: B)(implicit is: IndexingSemigroup[T]): is.|+|[A, B] = is.combine
+      def |+|[B <: X[T], ZZ[_, _]](b: B)(implicit i: ZZ[A, B], is: IndexingSemigroup[T, ZZ]): is.|+|[A, B] = is.combine
     }
   }
 }
