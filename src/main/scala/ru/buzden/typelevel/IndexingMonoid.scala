634b3c93670c3895b7df0b462f0445a418d12969
Denis Buzdalov <public@buzden.ru>
2019-02-20 15:25:47 +0300

    Indexing monoid was also split to typelevel proto and valu-level emerger

--- src/main/scala/ru/buzden/typelevel/IndexingMonoid.scala
+++ src/main/scala/ru/buzden/typelevel/IndexingMonoid.scala
@@ -2,11 +2,17 @@ package ru.buzden.typelevel
 
 trait IndexingMonoid[I] extends IndexingSemigroup[I] {
   type Empty <: I
+}
+
+trait IndexingMonoidEmerger[I, A <: I, B <: I] extends IndexingSemigroupEmerger[I, A, B] {
+  override val proto: IndexingMonoid[I]
+  import proto._
+
   def empty: Empty
 }
 
 object IndexingMonoid {
   object syntax {
-    def empty[I](implicit im: IndexingMonoid[I]): im.Empty = im.empty
+    def empty[I, A <: I, B <: I](implicit ime: IndexingMonoidEmerger[I, A, B]): ime.proto.Empty = ime.empty
   }
 }
