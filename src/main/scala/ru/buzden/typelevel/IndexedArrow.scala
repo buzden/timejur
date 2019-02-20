e2cfc824224b566b4fa8aed6d5920d31a309d67b
Denis Buzdalov <public@buzden.ru>
2019-02-20 14:55:39 +0300

    Number of type holes was reduced like it was done for indexed monad.

--- src/main/scala/ru/buzden/typelevel/IndexedArrow.scala
+++ src/main/scala/ru/buzden/typelevel/IndexedArrow.scala
@@ -1,12 +1,12 @@
 package ru.buzden.typelevel
 
-trait IndexedArrow[I, F[_, _, J, _ <: J]] {
+trait IndexedArrow[I, F[_, _, _]] {
   val im: IndexingMonoidZZ[I]
   import im._
 
-  def lift[A, B](f: A => B): F[A, B, I, Empty]
+  def lift[A, B](f: A => B): F[A, B, Empty]
 
-  def first[A, B, C, I_AB](fa: F[A, B, I, I_AB]): F[(A, C), (B, C), I, I_AB]
+  def first[A, B, C, I_AB](fa: F[A, B, I_AB]): F[(A, C), (B, C), I_AB]
 
-  def compose[A, B, C, I_AB, I_BC](f: F[B, C, I, I_BC], g: F[A, B, I, I_AB]): F[A, C, I, I_AB |+| I_BC]
+  def compose[A, B, C, I_AB, I_BC](f: F[B, C, I_BC], g: F[A, B, I_AB]): F[A, C, I_AB |+| I_BC]
 }
