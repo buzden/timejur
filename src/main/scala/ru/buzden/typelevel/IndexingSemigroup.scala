1aab4d3aa6cab8773fd63946726c2ede5f31ceb1
Denis Buzdalov <public@buzden.ru>
2019-02-20 15:16:46 +0300

    Typelevel and value-level semigroup combinations were splitted.

--- src/main/scala/ru/buzden/typelevel/IndexingSemigroup.scala
+++ src/main/scala/ru/buzden/typelevel/IndexingSemigroup.scala
@@ -1,38 +1,20 @@
 package ru.buzden.typelevel
 
-trait IndexingSemigroup[I] extends IndexingSemigroupZZ[I] {
-  import ru.buzden.typelevel.IndexingSemigroup.NewTypeUnit
-  type ZZ[A, B] = NewTypeUnit
+trait IndexingSemigroup[I] {
+  type |+|[A <: I, B <: I] <: I
+}
 
-  def combine[A <: I, B <: I]: A |+| B
+trait IndexingSemigroupCombiner[I, A <: I, B <: I] {
+  val is: IndexingSemigroup[I]
+  import is.|+|
 
-  final def combineZZ[A <: I, B <: I](implicit zz: ZZ[A, B]): A |+| B = combine[A, B]
+  def combine: A |+| B
 }
 
 object IndexingSemigroup {
-  case class NewTypeUnit(u: Unit) extends AnyVal
-  implicit val newTypeUnit: NewTypeUnit = NewTypeUnit(())
-
   object syntax {
     implicit class IndexedSemigroupOps[I, A <: I](val a: A) extends AnyVal {
-      def |+|[B <: I](b: B)(implicit is: IndexingSemigroupZZ[I]) = new CombineCreation[I, A, B]
-
-      def |+|[B <: I](b: B)(implicit is: IndexingSemigroup[I]): is.|+|[A, B] = is.combine[A, B]
-    }
-
-    // Workaround of lack of multiple implicit arguments lists
-    class CombineCreation[I, A <: I, B <: I](implicit val is: IndexingSemigroupZZ[I]) {
-      import is._
-      def apply(implicit zz: is.ZZ[A, B]): A |+| B = is.combineZZ
+      def |+|[B <: I](b: B)(implicit isc: IndexingSemigroupCombiner[I, A, B]): isc.is.|+|[A, B] = isc.combine
     }
   }
-}
-
-trait IndexingSemigroupZZ[I] {
-  // ZZ parameter is a cheating thing, a typelevel ability to require implicit of this type during combining.
-  type ZZ[_, _]
-
-  type |+|[A <: I, B <: I] <: I
-
-  def combineZZ[A <: I, B <: I](implicit zz: ZZ[A, B]): A |+| B
 }
