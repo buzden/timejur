3ab1bbb44a41d80cc0929206cf6079354b0a2f90
Denis Buzdalov <public@buzden.ru>
2019-02-21 00:59:51 +0300

    Simplification of the typedly timed computation class.

--- src/main/scala/ru/buzden/timejur/computation/TypedlyTimed.scala
+++ src/main/scala/ru/buzden/timejur/computation/TypedlyTimed.scala
@@ -1,10 +1,3 @@
 package ru.buzden.timejur.computation
 
-import ru.buzden.timejur.computation.TypedlyTimed.<=
-
-// todo MaxT should be a literal type
-final case class TypedlyTimed[-A, +B, T, MaxT <: T](f: A => (B, T <= MaxT))
-
-object TypedlyTimed {
-  type <=[A, B] = A Refined LessEqual[B]
-}
+final case class TypedlyTimed[-A, +B, T, MaxT <: T](f: A => (B, T Refined LessEqual[MaxT]))
