1e6d11bf2cd9c194b77eaef0032a8af1875cfcc3
Denis Buzdalov <public@buzden.ru>
2019-02-20 15:17:08 +0300

    `IndexingMonoidZZ` was removed due to removing of `IndexingSemigroupZZ`.

--- src/main/scala/ru/buzden/typelevel/IndexingMonoid.scala
+++ src/main/scala/ru/buzden/typelevel/IndexingMonoid.scala
@@ -1,14 +1,12 @@
 package ru.buzden.typelevel
 
-trait IndexingMonoid[I] extends IndexingMonoidZZ[I] with IndexingSemigroup[I]
-
-trait IndexingMonoidZZ[I] extends IndexingSemigroupZZ[I] {
+trait IndexingMonoid[I] extends IndexingSemigroup[I] {
   type Empty <: I
   def empty: Empty
 }
 
 object IndexingMonoid {
   object syntax {
-    def empty[I](implicit im: IndexingMonoidZZ[I]): im.Empty = im.empty
+    def empty[I](implicit im: IndexingMonoid[I]): im.Empty = im.empty
   }
 }
