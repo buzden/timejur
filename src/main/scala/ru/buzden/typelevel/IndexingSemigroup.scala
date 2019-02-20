595b664b8c260d558e886dce5c572f5e5c9ecab3
Denis Buzdalov <public@buzden.ru>
2019-02-20 13:09:24 +0300

    Refactoring: index type parameter was renamed, `T` -> `I`.

--- src/main/scala/ru/buzden/typelevel/IndexingSemigroup.scala
+++ src/main/scala/ru/buzden/typelevel/IndexingSemigroup.scala
@@ -1,12 +1,12 @@
 package ru.buzden.typelevel
 
-trait IndexingSemigroup[T] extends IndexingSemigroupZZ[T] {
+trait IndexingSemigroup[I] extends IndexingSemigroupZZ[I] {
   import ru.buzden.typelevel.IndexingSemigroup.NewTypeUnit
   type ZZ[A, B] = NewTypeUnit
 
-  def combine[A <: T, B <: T]: A |+| B
+  def combine[A <: I, B <: I]: A |+| B
 
-  final def combineZZ[A <: T, B <: T](implicit zz: ZZ[A, B]): A |+| B = combine[A, B]
+  final def combineZZ[A <: I, B <: I](implicit zz: ZZ[A, B]): A |+| B = combine[A, B]
 }
 
 object IndexingSemigroup {
@@ -14,25 +14,25 @@ object IndexingSemigroup {
   implicit val newTypeUnit: NewTypeUnit = NewTypeUnit(())
 
   object syntax {
-    implicit class IndexedSemigroupOps[T, A <: T](val a: A) extends AnyVal {
-      def |+|[B <: T](b: B)(implicit is: IndexingSemigroupZZ[T]) = new CombineCreation[T, A, B]
+    implicit class IndexedSemigroupOps[I, A <: I](val a: A) extends AnyVal {
+      def |+|[B <: I](b: B)(implicit is: IndexingSemigroupZZ[I]) = new CombineCreation[I, A, B]
 
-      def |+|[B <: T](b: B)(implicit is: IndexingSemigroup[T]): is.|+|[A, B] = is.combine[A, B]
+      def |+|[B <: I](b: B)(implicit is: IndexingSemigroup[I]): is.|+|[A, B] = is.combine[A, B]
     }
 
     // Workaround of lack of multiple implicit arguments lists
-    class CombineCreation[T, A <: T, B <: T](implicit val is: IndexingSemigroupZZ[T]) {
+    class CombineCreation[I, A <: I, B <: I](implicit val is: IndexingSemigroupZZ[I]) {
       import is._
       def apply(implicit zz: is.ZZ[A, B]): A |+| B = is.combineZZ
     }
   }
 }
 
-trait IndexingSemigroupZZ[T] {
+trait IndexingSemigroupZZ[I] {
   // ZZ parameter is a cheating thing, a typelevel ability to require implicit of this type during combining.
   type ZZ[_, _]
 
-  type |+|[A <: T, B <: T] <: T
+  type |+|[A <: I, B <: I] <: I
 
-  def combineZZ[A <: T, B <: T](implicit zz: ZZ[A, B]): A |+| B
+  def combineZZ[A <: I, B <: I](implicit zz: ZZ[A, B]): A |+| B
 }
