ce12cc006036418f9604f46764c6d27d27341efe
Denis Buzdalov <public@buzden.ru>
2019-02-20 14:58:49 +0300

    Proper type bounds were added.

--- src/main/scala/ru/buzden/typelevel/IndexedArrow.scala
+++ src/main/scala/ru/buzden/typelevel/IndexedArrow.scala
@@ -1,12 +1,12 @@
 package ru.buzden.typelevel
 
-trait IndexedArrow[I, F[_, _, _]] {
+trait IndexedArrow[I, F[_, _, _ <: I]] {
   val combiner: IndexingMonoidZZ[I]
   import combiner._
 
   def lift[A, B](f: A => B): F[A, B, Empty]
 
-  def first[A, B, C, I_AB](fa: F[A, B, I_AB]): F[(A, C), (B, C), I_AB]
+  def first[A, B, C, I_AB <: I](fa: F[A, B, I_AB]): F[(A, C), (B, C), I_AB]
 
-  def compose[A, B, C, I_AB, I_BC](f: F[B, C, I_BC], g: F[A, B, I_AB]): F[A, C, I_AB |+| I_BC]
+  def compose[A, B, C, I_AB <: I, I_BC <: I](f: F[B, C, I_BC], g: F[A, B, I_AB]): F[A, C, I_AB |+| I_BC]
 }
