dcb6607be4cbb7300630eed3e3b1ba264cdd2f6b
Denis Buzdalov <public@buzden.ru>
2019-02-23 00:39:34 +0300

    Arrow instance was added for the IFT.

--- src/main/scala/ru/buzden/typelevel/IFT.scala
+++ src/main/scala/ru/buzden/typelevel/IFT.scala
@@ -1,5 +1,10 @@
 package ru.buzden.typelevel
 
+import cats.arrow.ArrowChoice
+import cats.instances.function._
+import cats.syntax.arrowChoice._
+import cats.syntax.compose._
+import cats.syntax.strong._
 import cats.{Contravariant, Functor}
 
 /** Ersatz for implicit function type */
@@ -14,5 +19,12 @@ object IFT {
 
   implicit def iftContravariantFunctor[Y]: Contravariant[IFT[?, Y]] = new Contravariant[IFT[?, Y]] {
     override def contramap[A, B](fa: IFT[A, Y])(f: B => A): IFT[B, Y] = IFT(fa.f `compose` f)
+  }
+
+  implicit def iftArrowChoice: ArrowChoice[IFT] = new ArrowChoice[IFT] {
+    override def lift[A, B](f: A => B): IFT[A, B] = IFT(f)
+    override def compose[A, B, C](f: IFT[B, C], g: IFT[A, B]): IFT[A, C] = IFT(f.f <<< g.f)
+    override def first[A, B, C](fa: IFT[A, B]): IFT[(A, C), (B, C)] = IFT(fa.f.first)
+    override def choose[A, B, C, D](f: IFT[A, C])(g: IFT[B, D]): IFT[Either[A, B], Either[C, D]] = IFT(f.f +++ g.f)
   }
 }
