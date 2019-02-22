9999399be27cabc8077b0da6ad2840215d595c63
Denis Buzdalov <public@buzden.ru>
2019-02-23 00:48:33 +0300

    Particular case of indexed monad was added for code shortening.

--- src/main/scala/ru/buzden/typelevel/IndexedMonad.scala
+++ src/main/scala/ru/buzden/typelevel/IndexedMonad.scala
@@ -27,6 +27,13 @@ trait IndexedMonad[I, F[_, _ <: I]] {
   def flatMap[A, I_A <: I, B, I_B <: I](fa: F[A, I_A])(f: A => F[B, I_B]): FlatMapR[F[B, I_A |+| I_B]]
 }
 
+abstract class SimpleIndexedMonad[I: IndexingMonoid, F[_, _ <: I]] extends IndexedMonad[I, F] {
+  override val im: IndexingMonoid[I] = implicitly
+
+  override type PureR[A] = A
+  override type FlatMapR[A] = A
+}
+
 object IndexedMonad {
   object syntax {
     implicit class IndexedMonadAnyAOps[A](val a: A) extends AnyVal {
--- src/main/scala/ru/buzden/typelevel/instances.scala
+++ src/main/scala/ru/buzden/typelevel/instances.scala
@@ -29,11 +29,7 @@ object instances {
   }
 
   type I2A[M[_], A, B] = M[A]
-  implicit def monadIsIndexedMonad[I: IndexingMonoid, M[_]: Monad]: IndexedMonad[I, I2A[M, ?, ?]] = new IndexedMonad[I, I2A[M, ?, ?]] {
-    override type PureR[A] = A
-    override type FlatMapR[A] = A
-    override val im: IndexingMonoid[I] = implicitly
-
+  implicit def monadIsIndexedMonad[I: IndexingMonoid, M[_]: Monad]: IndexedMonad[I, I2A[M, ?, ?]] = new SimpleIndexedMonad[I, I2A[M, ?, ?]] {
     override def pure[A](a: A): M[A] = Monad[M].pure(a)
     override def flatMap[A, I_A <: I, B, I_B <: I](fa: M[A])(f: A => M[B]): M[B] = Monad[M].flatMap(fa)(f)
   }
