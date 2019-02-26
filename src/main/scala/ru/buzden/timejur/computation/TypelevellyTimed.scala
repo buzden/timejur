94b4d283b632e6b18e1c0c20f730c590c48cc659
Denis Buzdalov <public@buzden.ru>
2019-02-26 15:14:19 +0300

    Small renaming from an ugly non-existent word to another non-existent.

--- src/main/scala/ru/buzden/timejur/computation/TypelevellyTimed.scala
+++ src/main/scala/ru/buzden/timejur/computation/TypelevellyTimed.scala
@@ -3,4 +3,4 @@ package ru.buzden.timejur.computation
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.numeric.LessEqual
 
-final case class TypedlyTimed[-A, +B, T, MaxT <: T](f: A => (B, T Refined LessEqual[MaxT]))
+final case class TypelevellyTimed[-A, +B, T, MaxT <: T](f: A => (B, T Refined LessEqual[MaxT]))
