6012c1505a06057bb308a5fa4fc870358bd24aaf
Denis Buzdalov <public@buzden.ru>
2019-02-26 13:36:15 +0300

    Refined library was added as a dependency.

--- src/main/scala/ru/buzden/timejur/computation/TypedlyTimed.scala
+++ src/main/scala/ru/buzden/timejur/computation/TypedlyTimed.scala
@@ -1,3 +1,6 @@
 package ru.buzden.timejur.computation
 
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.LessEqual
+
 final case class TypedlyTimed[-A, +B, T, MaxT <: T](f: A => (B, T Refined LessEqual[MaxT]))
