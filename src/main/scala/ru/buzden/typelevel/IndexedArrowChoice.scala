f3738c3f8523d76559695f87d62c38d2d2223618
Denis Buzdalov <public@buzden.ru>
2019-02-26 17:33:00 +0300

    Additional resulting type fields were added to indexed arrow typeclasses

--- src/main/scala/ru/buzden/typelevel/IndexedArrowChoice.scala
+++ src/main/scala/ru/buzden/typelevel/IndexedArrowChoice.scala
@@ -4,5 +4,7 @@ trait IndexedArrowChoice[I, F[_, _, _ <: I]] extends IndexedArrow[I, F] {
   val chooser: IndexingSemigroup[I]
   import chooser._
 
-  def choose[A, B, C, D, I_AC <: I, I_BD <: I](f: F[A, C, I_AC])(g: F[B, D, I_BD]): F[Either[A, B], Either[C, D], I_AC |+| I_BD]
+  type ChooseR[I_AC, I_BD, R]
+
+  def choose[A, B, C, D, I_AC <: I, I_BD <: I](f: F[A, C, I_AC])(g: F[B, D, I_BD]): ChooseR[I_AC, I_BD, F[Either[A, B], Either[C, D], I_AC |+| I_BD]]
 }
