9ff9f4a3b868a4fcaf1c6bd39050a051078f6d12
Denis Buzdalov <public@buzden.ru>
2019-02-27 18:54:05 +0300

    Indexed timed values were made to have `Iz` prefix.

---
+++ src/main/scala/ru/buzden/timejur/value/IzTimedValue.scala
@@ -0,0 +1,23 @@
+package ru.buzden.timejur.value
+
+import ru.buzden.iz._
+
+/** Timed value that contains time only at the type level */
+final case class IzTimedValue[A, ATime](value: A)
+
+object IzTimedValue {
+  implicit def indexedMonadForIndexedTimedValue[T: TLMonoid]: IzMonad[T, IzTimedValue] = new IzMonad[T, IzTimedValue] {
+    override val im: TLMonoid[T] = implicitly
+    import im._
+
+    override type PureR[A] = A
+    override type FlatMapR[ATime, BTime, C] = C
+
+    override def pure[A](a: A): IzTimedValue[A, Empty] =
+      IzTimedValue[A, Empty](a)
+
+    override def flatMap[A, ATime <: T, B, BTime <: T]
+    (fa: IzTimedValue[A, ATime])(f: A => IzTimedValue[B, BTime]): IzTimedValue[B, ATime |+| BTime] =
+      IzTimedValue[B, ATime |+| BTime](f(fa.value).value)
+  }
+}
