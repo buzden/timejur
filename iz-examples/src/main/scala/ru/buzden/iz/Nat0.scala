edc5ae28ab2af612e994cddc5c1e74d6803617f9
Denis Buzdalov <public@buzden.ru>
2019-03-01 20:36:04 +0300

    Basic `Nat0` type with the type-level addition was added.

---
+++ iz-examples/src/main/scala/ru/buzden/iz/Nat0.scala
@@ -0,0 +1,15 @@
+package ru.buzden.iz
+
+sealed trait Nat0 {
+  type Sum[N <: Nat0] <: Nat0
+}
+
+object Nat0 {
+  final case object Zero extends Nat0 {
+    override type Sum[N <: Nat0] = N
+  }
+
+  final case class Succ[Prev <: Nat0](p: Prev) extends Nat0 {
+    override type Sum[N <: Nat0] = Succ[p.Sum[N]]
+  }
+}
