af92908dbf86f951faed22d54154a679bdcdee82
Denis Buzdalov <public@buzden.ru>
2019-03-04 16:01:52 +0300

    Value-level addition method was added to `Nat0`.

--- iz-examples/src/main/scala/ru/buzden/iz/Nat0.scala
+++ iz-examples/src/main/scala/ru/buzden/iz/Nat0.scala
@@ -2,15 +2,18 @@ package ru.buzden.iz
 
 sealed trait Nat0 {
   type Sum[N <: Nat0] <: Nat0
+  def add[N <: Nat0](n: N): Sum[N]
 }
 
 object Nat0 {
   final case object Zero extends Nat0 {
     override type Sum[N <: Nat0] = N
+    override def add[N <: Nat0](n: N): Sum[N] = n
   }
 
   final case class Succ[Prev <: Nat0](p: Prev) extends Nat0 {
     override type Sum[N <: Nat0] = Succ[p.Sum[N]]
+    override def add[N <: Nat0](n: N): Succ[p.Sum[N]] = Succ(p.add(n))
   }
 
   implicit val nat0HasTLMonoid: TypeLevelMonoid[Nat0] = new TypeLevelMonoid[Nat0] {
