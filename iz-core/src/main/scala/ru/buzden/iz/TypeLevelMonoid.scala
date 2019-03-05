a623b8924b3fdb922ed14ec35c61de6a83c6aa37
Denis Buzdalov <public@buzden.ru>
2019-03-05 15:29:30 +0300

    Return types were removed from type-level semigroups and monoids.

--- iz-core/src/main/scala/ru/buzden/iz/TypeLevelMonoid.scala
+++ iz-core/src/main/scala/ru/buzden/iz/TypeLevelMonoid.scala
@@ -7,14 +7,11 @@ trait TypeLevelMonoid[I] extends TypeLevelSemigroup[I] {
 
 /** Type-level monoid with an ability to emerge appropriate value */
 trait TwoFacedMonoid[I] extends TwoFacedSemigroup[I] with TypeLevelMonoid[I] {
-  /** Type of the result of the `empty` operation. */
-  type EmptyR[_]
-
-  def empty: EmptyR[Empty]
+  def empty: Empty
 }
 
 object TypeLevelMonoid {
   object syntax {
-    def empty[I, A <: I, B <: I](implicit eim: TwoFacedMonoid[I]): eim.EmptyR[eim.Empty] = eim.empty
+    def empty[I, A <: I, B <: I](implicit eim: TwoFacedMonoid[I]): eim.Empty = eim.empty
   }
 }
--- iz-examples/src/main/scala/ru/buzden/iz/instances/unit.scala
+++ iz-examples/src/main/scala/ru/buzden/iz/instances/unit.scala
@@ -4,9 +4,6 @@ import ru.buzden.iz.TwoFacedMonoid
 
 object unit {
   implicit val unitHasIndexingMonoid: TwoFacedMonoid[Unit] = new TwoFacedMonoid[Unit] {
-    override type EmptyR[A] = A
-    override type CombineR[A, B, C] = C
-
     override type Empty = Unit
     override type |+|[A, B] = Unit
 
